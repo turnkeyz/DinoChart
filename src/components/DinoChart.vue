20ea1b3bea61060783712edf5be0e4d0dfe6c5e8
turnkeyz <34150538+turnkeyz@users.noreply.github.com>
2022-10-29 17:00:26 -0500

    some styling for the chart containers

--- src/components/DinoChart.vue
+++ src/components/DinoChart.vue
@@ -1,7 +1,32 @@
 <template>
 <main>
-    <div id="test">
-        <p>stuff goes here</p>
+    <div class="container" id="chartOne">
+        <div class="col mt-3">
+            <p>First Chart</p>
+            <div class="input-group mb-3">
+            </div>
+        </div>
+    </div>
+    <div class="container" id="chartTwo">
+        <div class="col mt-3">
+            <p>Second Chart</p>
+            <div class="input-group mb-3">
+            </div>
+        </div>
+    </div>
+    <div class="container" id="chartThree">
+        <div class="col mt-3">
+            <p>Third Chart</p>
+            <div class="input-group mb-3">
+            </div>
+        </div>
+    </div>
+    <div class="container" id="chartFour">
+        <div class="col mt-3">
+            <p>Fourth Chart</p>
+            <div class="input-group mb-3">
+            </div>
+        </div>
     </div>
 </main>
 </template>
@@ -16,3 +41,34 @@ export default {
   },
 };
 </script>
+<style scoped>
+.chartOne{
+    display: inline-block;
+    width: 20px;
+    height: 75px;
+    background-color: teal;
+    border: 1px solid black;
+    padding: 50px;
+}
+.chartTwo{
+    display: inline-block;
+    width: 20px;
+    height: 75px;
+    background-color: teal;
+    border: 1px solid black;
+}
+.chartThree{
+    display: inline-block;
+    width: 20px;
+    height: 75px;
+    background-color: teal;
+    border: 1px solid black;
+}
+.chartFour{
+    display: inline-block;
+    width: 20px;
+    height: 75px;
+    background-color: teal;
+    border: 1px solid black;
+}
+</style>

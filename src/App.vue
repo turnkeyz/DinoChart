3c942e8c15ceafe3fb16544da2190493ac13a735
turnkeyz <34150538+turnkeyz@users.noreply.github.com>
2022-10-29 20:36:01 -0500

    my changes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <div class="text-center">
-        <h2 class="text-center mt-5">Dynamic Fuel and Production Charts</h2>
-        <p> Trending data for the production cycles of Fossil Fuels!
-        </p>
-      <DinoChart />
-      </div>
-    </div>
+  <div id="app" class="text-center mt-5">
+    <h2>Dynamic Fuel and Production Charts</h2>
+        <p> Trending data for the production cycles of Fossil Fuels!</p>
+    <DinoChart/>
   </div>
 </template>
 
 <script>
 import DinoChart from './components/DinoChart.vue'
-
+ 
 export default {
   name: 'App',
   components: {
@@ -21,14 +16,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/DinoChart.vue
+++ src/components/DinoChart.vue
@@ -1,74 +1,23 @@
 <template>
-<main>
-    <div class="container" id="chartOne">
-        <div class="col mt-3">
-            <p>First Chart</p>
-            <div class="input-group mb-3">
-            </div>
-        </div>
+    <div>
+      <canvas id="ChartOne"></canvas>
     </div>
-    <div class="container" id="chartTwo">
-        <div class="col mt-3">
-            <p>Second Chart</p>
-            <div class="input-group mb-3">
-            </div>
-        </div>
-    </div>
-    <div class="container" id="chartThree">
-        <div class="col mt-3">
-            <p>Third Chart</p>
-            <div class="input-group mb-3">
-            </div>
-        </div>
-    </div>
-    <div class="container" id="chartFour">
-        <div class="col mt-3">
-            <p>Fourth Chart</p>
-            <div class="input-group mb-3">
-            </div>
-        </div>
-    </div>
-</main>
-</template>
-<script>
-export default {
-  name: "DinoChart",
-  props: {
-    Chart: {
-      type: Object,
-      required: true,
+  </template>
+  
+  <script>
+  import Chart from 'chart.js';
+  import chartOneData from '../ChartOne.js';
+
+  export default {
+    name: 'ChartOne',
+    data(){
+        return{
+            chartOneData:chartOneData
+        }
     },
-  },
-};
-</script>
-<style scoped>
-.chartOne{
-    display: inline-block;
-    width: 20px;
-    height: 75px;
-    background-color: teal;
-    border: 1px solid black;
-    padding: 50px;
-}
-.chartTwo{
-    display: inline-block;
-    width: 20px;
-    height: 75px;
-    background-color: teal;
-    border: 1px solid black;
-}
-.chartThree{
-    display: inline-block;
-    width: 20px;
-    height: 75px;
-    background-color: teal;
-    border: 1px solid black;
-}
-.chartFour{
-    display: inline-block;
-    width: 20px;
-    height: 75px;
-    background-color: teal;
-    border: 1px solid black;
-}
-</style>
+    mounted(){
+        const ctx = document.getElementById('ChartOne');
+        new Chart(ctx, this.chartOneData)
+    }
+  }
+  </script>
